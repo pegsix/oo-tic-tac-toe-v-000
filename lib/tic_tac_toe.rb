aa41e27de50d91a88debd27edfa2a96091e0d158
Peggy Lee <peggyleesix@gmail.com>
2018-06-13 22:26:29 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -14,13 +14,19 @@ WIN_COMBINATIONS = [
   [2,4,6]  # 2nd diagonal win_combination[7]
 ]
 
-  def display_board(board)
+  def display_board
     puts " #{board[0]} | #{board[1]} | #{board[2]} "
     puts "-----------"
     puts " #{board[3]} | #{board[4]} | #{board[5]} "
     puts "-----------"
     puts " #{board[6]} | #{board[7]} | #{board[8]} "
   end
+
+  def position_taken?(board, index)
+     !(board[index].nil? || board[index] == " ")
+   end
+  #
+  
 end
 
 # def display_board(board)
