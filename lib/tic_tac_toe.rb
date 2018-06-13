2912f831b6dc2b095b1608c4dfb4ba68074cc22b
Peggy Lee <peggyleesix@gmail.com>
2018-06-13 22:04:06 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -4,7 +4,16 @@ class TicTacToe
   end
 end
 
-
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row win_combination[0]
+  [3,4,5],  # Middle row win_combination[1]
+  [6,7,8],  # Bottom row win_combination[2]
+  [0,3,6],  # Left column win_combination[3]
+  [1,4,7],  # Middle column win_combination[4]
+  [2,5,8],  # Right column win_combination[5]
+  [0,4,8],  # 1st diagonal win_combination[6]
+  [2,4,6]  # 2nd diagonal win_combination[7]
+]
 
 # def display_board(board)
 #   puts " #{board[0]} | #{board[1]} | #{board[2]} "
