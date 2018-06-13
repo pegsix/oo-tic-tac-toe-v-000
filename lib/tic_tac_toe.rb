3ea2d0363704104696d5b306dcbc08437a3a1dd8
Peggy Lee <peggyleesix@gmail.com>
2018-06-13 22:28:01 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -14,7 +14,7 @@ WIN_COMBINATIONS = [
   [2,4,6]  # 2nd diagonal win_combination[7]
 ]
 
-  def @board
+  def display_board
     puts " #{board[0]} | #{board[1]} | #{board[2]} "
     puts "-----------"
     puts " #{board[3]} | #{board[4]} | #{board[5]} "
